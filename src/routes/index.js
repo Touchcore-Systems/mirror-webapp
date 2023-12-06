0eab3d6624fd3c6401e88742acf734a2cd31ed6e
Roshni-Touchcore <roshniv@touchcoresystems.com>
2023-12-06 21:56:42 +0530

    changed path to repo name

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const router = createRouter({
     routes: [
         {
             // path: "/",
-            path: "/mirror-webapp",
+            path: "/mirror-webapp/",
             component: AuthLayout,
             children:[
                 {
